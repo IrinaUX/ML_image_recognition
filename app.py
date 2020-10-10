4cd2fc72e52d150c9e3f6a5d25afda1951d5ee11
Irina Kim <irinahpda@gmail.com>
2020-10-10 11:34:20 +0100

    Add keras to requirements.

--- app.py
+++ app.py
@@ -1,45 +1,16 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-# import scrape_mars
+from keras.models import load_model
 import pymongo
+
 # from secret import username, password
 
 app = Flask(__name__)
 
-# Create connection variable
-## (IK) conn = 'mongodb://localhost:27017'
-## (IK) conn = f'mongodb+srv://{username}:{password}@ml-mongo-db.p34ii.mongodb.net'
-# conn = f'mongodb+srv://{username}:{password}@cluster0.p34ii.mongodb.net'
-
-# Pass connection to the pymongo instance.
-# client = pymongo.MongoClient(conn)
-
-# Connect to a database. Will create one if not already available.
-##(IK)db = client.mars_db
-# db = client.Cluster0
-
-# # Drops collection if available to remove duplicates
-# db.intro.drop()
-# db.test.drop() # <----- DELETE
-# db.feat_img.drop()
-
-# # Insert data into the database
-# db.intro.insert([{'title': '', 'body': '', 'url': "",
-#     'weather': ""}])
-
-# # Check if second dictionary can be added
-# feat_img = db.feat_img
-# feat_img.insert_many([{"name": "", "link": ""}])
-
-
-# # Use flask_pymongo to set up mongo connection
-# ##(IK) app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_db"
-# uri = f'mongodb+srv://{username}:{password}@cluster0.p34ii.mongodb.net/intro'
-# app.config["MONGO_URI"] = uri
-# mongo = PyMongo(app)
-
-# Or set inline
-# mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_db")
+# Load the model
+from keras.models import load_model
+model = load_model('mnist.h5')
+model = 
 
 @app.route('/')
 def hello_world():
