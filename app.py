a752d188798e4d10be65a4eb8a6cec382cced554
Tunde Adebayo <highmic2407@gmail.com>
2020-10-17 10:14:37 -0500

    Update app.py
    updated

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 import sys
 from flask import Flask, redirect, request, render_template,jsonify
 from process_predict import predict_new, base64_to_pil
+from gevent.pywsgi import WSGIServer
 
 app = Flask(__name__)
 
@@ -28,5 +29,9 @@ def draw():
     return render_template('draw.html')
 
 if __name__ == '__main__':
-    app.run()
+    # Serve the app with gevent
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
+    http_server.serve_forever()
+ 
+   
  
