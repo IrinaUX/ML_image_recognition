49fa37c476165715b5b049a1ada5df9df7105bb2
Irina Kim <irinahpda@gmail.com>
2020-10-20 17:44:44 +0100

    Cleanup to free space for heroku deployment.

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import sys
 
 # Flask
 from flask import Flask, redirect, url_for, request, render_template, Response, jsonify, redirect
-from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
+# from werkzeug.utils import secure_filename
+# from gevent.pywsgi import WSGIServer
 
 # TensorFlow and tf.keras
-import tensorflow as tf
-from tensorflow import keras
+# import tensorflow as tf
+# from tensorflow import keras
 
 from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from tensorflow.keras.models import load_model
